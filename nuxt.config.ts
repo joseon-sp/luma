07dae2a27b9a65b4bb1531659533c04e42cba5cc
SuhJae <jwsuh25@kis.ac>
2024-11-30 23:59:41 +0900

    fix: supabase url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,8 @@ export default defineNuxtConfig({
   modules: ["@nuxt/ui", "@nuxt/image", "@nuxt/fonts", "@nuxtjs/supabase"],
 
   supabase: {
+    url: "https://nhighfjqqrkbcwabajpo.supabase.co",
+    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oaWdoZmpxcXJrYmN3YWJhanBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE0NjcwMjMsImV4cCI6MjA0NzA0MzAyM30.SNykYLw5LMWBwTPtzxniXLNYJQtqYzwSaeZkbJJFkVY",
     redirectOptions: {
       login: "/login",
       callback: "/confirm",
